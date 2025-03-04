cc4345da978430581101b0898603ca205723e35f
arkstudiodevelopers <nicksonogm@gmail.com>
2025-03-04 10:17:08 +0300

    created the design of the navigation bar

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,24 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-
+  margin: 0;
+  padding: 0;
 }
+
+.container {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
 </style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav>
+    <nav class="navigation-bar">
         <ul>
             <li>Portfolio</li>
             <li>Covid 19 summary</li>
@@ -19,9 +19,46 @@
 
     methods: {
     }
-    
+
   };
   </script>
   
   <style scoped>
+  .navigation-bar{
+    background-color: #063970;
+    height: 70px;
+    width: 100%;
+    margin: 0;
+    border: 0;
+    padding: 0;
+  }
+
+.navigation-bar ul{
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: space-between; /* Key property */
+    width: 100%;
+    height: 100%;
+    align-items: center;
+}
+
+.navigation-bar li {
+    /* display: inline-block; Makes list items horizontal */
+    padding: 20px; /* Adjust as needed */
+    color: white; /* For better visibility */
+    list-style: none;
+}
+
+.navigation-bar li:first-child {
+  margin-right: auto;
+}
+
+.navigation-bar li:last-child {
+  margin-left: auto;
+}
+
+.navigation-bar li:nth-child(2) {
+  margin: 0 auto;
+}
   </style>
